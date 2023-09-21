56d49796824aeae3f5e9c4dfa8047e533391b06c
Hrishikesh Patil <hrishikesh.patil@yandex.com>
2023-09-22 00:59:56 +0530

    spring boot

---
+++ src/main/java/com/bugtracker/pareshaan/PareshaanApplication.java
@@ -0,0 +1,13 @@
+package com.bugtracker.pareshaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PareshaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PareshaanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bugtracker/pareshaan/PareshaanApplicationTests.java
@@ -0,0 +1,9 @@
+package com.bugtracker.pareshaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PareshaanApplicationTests {
+
+}
